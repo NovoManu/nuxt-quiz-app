625b8f31e7d1126c857408e42740877b5167c7c4
NovoManu <operamike@mail.ru>
2019-04-09 19:08:00 +0300

    Initial setup

---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+  server: {
+    port: 9000
+  },
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  modules: ['@nuxtjs/axios', '@nuxtjs/bulma'],
+  axios: {
+    proxy: true,
+    proxyHeaders: true,
+    proxyHeadersIgnore: ['accept']
+  },
+  proxy: {
+    '/api': {
+      target: 'http://localhost:3000/',
+      changeOrigin: false,
+      pathRewrite: {
+        '^/api/': ''
+      }
+    }
+  },
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ store/index.js
@@ -0,0 +1,10 @@
+export const state = () => ({})
+export const actions = {
+  async nuxtServerInit({ dispatch, rootGetters }) {
+    // if (rootGetters['auth/authorized']) {
+    //   await dispatch('notifications/fetchServerNotifications', null, {
+    //     root: true
+    //   })
+    // }
+  }
+}
