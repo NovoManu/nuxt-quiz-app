66aa3ab6fce441e77ecb03fbf68d3de39d9100dd
Manu <manu@MacBook-Air-Manu.local>
2019-04-14 19:32:01 +0300

    score

--- pages/scores.vue
+++ pages/scores.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>Scores</div>
+  <div class="scores-page">
+    <div class="scores-page__content">
+      <div class="text-center">
+        <img src="~/static/img/star.png" />
+        <h1>Your Score: 4/5</h1>
+      </div>
+    </div>
+    <div class="scores-page__top-score">
+      <h1>Top 10</h1>
+      <div class="top-score__list">
+        <div class="top-score__list-item">
+          <strong>Name</strong>
+          <span>5/5</span>
+        </div>
+        <div class="top-score__list-item">
+          <strong>Name</strong>
+          <span>5/5</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {}
-  }
+  layout: 'main'
 }
 </script>
-
-<style scoped></style>
